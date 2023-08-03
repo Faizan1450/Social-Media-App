bd1605e541fb3ed790dcfd0899cc912615b6a866
Syed Faizan Ali <76842592+Faizan1450@users.noreply.github.com>
2023-08-03 14:21:00 +0530

    delete repeated directory

--- Social Media Website/script.js
+++
@@ -1,8 +0,0 @@
-let notifications = document.getElementById("notifications");
-let popup = document.querySelector(".left #notifications .notification-popup");
-notifications.addEventListener("click", ()=>{
-    if(popup.style.display === "block")
-        popup.style.display = "none"
-    else 
-        popup.style.display = "block"
-})
